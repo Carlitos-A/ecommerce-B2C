88819239e31d806d82dac4c873d6328444257019
Carlos Jose Camero Armenia <carlosjosearmenia@gmail.com>
2026-03-18 21:28:55 -0300

    Reaction.java getters and setters

--- reviews/src/main/java/dev/rampmaster/ecommerce/reviews/model/Reaction.java
+++ reviews/src/main/java/dev/rampmaster/ecommerce/reviews/model/Reaction.java
@@ -6,5 +6,38 @@ public class Reaction {
     private Long reviewId;
     private Long userId;
     private ReactionType type;
+
+        return id;
+    }
+
+    public Long getReviewId() {
+        return reviewId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public ReactionType getType() {
+        return type;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setReviewId(Long reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setType(ReactionType type) {
+        this.type = type;
+    }
+
     
+
 }
